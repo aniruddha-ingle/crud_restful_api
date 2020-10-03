a362bfb7d2bcfbb17ea9eb997f61f6580ea325e0
Aniruddha Ingle <30553773+aniruddha-ingle@users.noreply.github.com>
2020-10-03 16:30:16 +0530

    Doc String, Functionality Improvements
    Update Log:
    1. Doc Strings and Comments were added for documentational clarity.
    2. Error Messages were improved.
    3. Spaces within Query (which aren't even supported by the pymongo client) were handled.
    4. URL is no longer used as primary source of query transfer because it seems that spaces create resolution issues.

--- test_api.py
+++ test_api.py
@@ -23,30 +23,75 @@ products = db.gd_products
 ##
 
 def string_util_one(query_string):
-    s = query_string.split(',')
+    """
+    Parameters
+    ----------
+    query_string : string
+        All queries must be string in REST APIs, these queries are held by the
+        query string.
+
+    Returns
+    -------
+    d : dictionary
+        All the queries to the mongodb database use filters which accept dictionaries. 
+        This utility function accepts a string query and maps it to a dictionary according
+        to some conventions stated in the readme.md file.
+
+    """
+    if query_string == None:
+        abort(400 , 
+              message = 'Bad Request: Make sure you have used the correct argument,operation pair. Furthermore, make sure there is no space between the argument and the following equal to sign. Spaces after that are handled.')
+    
+    #In the above error message arguments refer to any one of the four added arguments
+    #E.g. record, filter, update, replacedby
+    #Each argument should be immediately followed by the equal to sign
+    #E.g. record=
+    #However, spaces in the quert after this equal to sign are handled.
+    
+    s = query_string.split(';')
     d = {}
     float_attrs = ['regular_price_value','offer_price_value']
     for attr in s:
-        pair = attr.split('=')
-        if pair[0] in float_attrs:
-            d[pair[0]] = float(pair[1])
+        pair = attr.split(':')
+        if pair[0].strip() in float_attrs:
+            d[pair[0].strip()] = float(pair[1].strip())
         else:
-            d[pair[0]] = pair[1]
+            d[pair[0].strip()] = pair[1].strip()
     return d
 
 ##
 #Using reqparser to specify special arguments within the URL
+#'record' is used to create a document before inserting it in the database
+#'filter' is used to query the database
 #'update' is used in the update operation
 #'replacedby' is used in the replace operation
 ##
+
 parser = reqparse.RequestParser()
+parser.add_argument('record')
+parser.add_argument('filter')
 parser.add_argument('update')
 parser.add_argument('replacedby')
 
 #View, Delete, or Update Product Documents
 class Products(Resource):
     
-    def get(self, search_key): #view
+    def get(self):
+        """
+        It is important to note that the default status of the GET function is not
+        functional for handling spaces in query fields like "name" in the product 
+        documents.
+        Please specify -X GET after the URL.
+
+        Returns
+        -------
+        result : json
+            A database read is performed based on a string query found in args['filter']
+            This result is converted to json and returned.
+
+        """
+        args = parser.parse_args()
+        search_key = args['filter']
         d_find = string_util_one(search_key)
         response = products.find_one(d_find)
         if response == None:
@@ -56,16 +101,31 @@ class Products(Resource):
         result = json.dumps({'result': response}, sort_keys=True )
         return result
     
-    def delete(self, search_key): #delete
+    def delete(self): #delete
+        """
+        Returns
+        -------
+        "1 product was deleted" iff a product was deleted
+        "0 product was deleted" iff a product was not deleted
+        """
+        args = parser.parse_args()
+        search_key = args['filter']
         d_find = string_util_one(search_key)
         del_res = products.delete_one(d_find)
         return '{} product was deleted'.format(del_res.deleted_count)
     
-    def put(self, search_key): #update
-        d_find = string_util_one(search_key)
+    def put(self): #update
+        """
+        Returns
+        -------
+        "1 product was updated" iff a product was deleted
+        "0 product was updated" iff a product was not deleted
+        """
         args = parser.parse_args()
-        s = args['update'].split(':')
-        s[0] = '$'+s[0]
+        search_key = args['filter']
+        d_find = string_util_one(search_key)   
+        s = args['update'].split('>')
+        s[0] = '$'+s[0].strip()
         d_up = string_util_one(s[1])
         d_update = {}
         d_update[s[0]] = d_up
@@ -74,14 +134,30 @@ class Products(Resource):
 
 #Create or Replace Product Documents
 class Create(Resource):
-    def post(self, insert_string): #create/insert a new product document
-        d_insert = string_util_one(insert_string)
-        products.insert_one(d_insert)
-        return 'Product was added to the database', 201
-    
-    def put(self, insert_string): #replace an existing product with a new product.
-        d_find = string_util_one(insert_string)
+
+    def post(self): #create/insert a new product document
+        """
+        Returns
+        -------
+        When a new document is added to mongodb it assigns it with an inbuilt Id
+        This id is displayed as confirmation of insert operation.
+        """
         args = parser.parse_args()
+        insert_string = args['record']
+        d_insert = string_util_one(insert_string)
+        in_res = products.insert_one(d_insert)
+        return 'Added Product has ID {}'.format(in_res.inserted_id), 201
+    
+    def put(self): #replace an existing product with a new product.
+        """
+        Returns
+        -------
+        "1 product was replaced" iff a product was replaced
+        "0 product was replaced" iff a product matching the search filter was not found
+        """
+        args = parser.parse_args()
+        search_key = args['filter']
+        d_find = string_util_one(search_key)
         d_rep = string_util_one(args['replacedby'])
         rep_res = products.replace_one(d_find,d_rep)
         return '{} product was replaced'.format(rep_res.modified_count)
@@ -89,8 +165,8 @@ class Create(Resource):
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(Create, '/create/<insert_string>')
-api.add_resource(Products, '/products/<search_key>')
+api.add_resource(Create, '/create/')
+api.add_resource(Products, '/products/')
 
 
 
